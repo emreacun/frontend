3b36adaeb1b6f71238c608395428cd4e87050736
Emre Acun <acunnemre@gmail.com>
2023-09-16 17:19:09 +0300

    Search, Dark Mode, Footer, Header

--- Front-end bootcamp/template.js
+++ Front-end bootcamp/template.js
@@ -1,1 +1,25 @@
-// alert("Merhaba")
+// Dark Mode
+let darkModu=()=>{
+    window.document.classList.toggle("dark_mode"); // toggle
+}
+
+// input search
+$(document).ready(function(){
+    const searchApi=["Adana", "Balıkesir", "Çorum", "Denizli", "Diyarbakır" , "Edirne", "Malatya", "Van"];
+    $("#tags").autocomplete({
+        source:searchApi
+    })
+}); //end
+
+
+// Footer
+let newDate=()=>{
+    const date=new Date().getFullYear();
+    // JS DOM
+    //document.getElementById("date_id").innerHTML=new Date().getFullYear();
+
+    // Jquery DOM
+    $("#date_id").html(date);
+}
+newDate()
+
